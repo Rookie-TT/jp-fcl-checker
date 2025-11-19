957fd53f9e91f89700a231fdebe29c111ac33535
Rookie-TT <44184051+Rookie-TT@users.noreply.github.com>
2025-11-19 09:42:45 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -119,7 +119,8 @@ def check():
         })
     
     return jsonify({"results": results})
-
 if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
 
-    app.run(host="0.0.0.0", port=5000, debug=True)
